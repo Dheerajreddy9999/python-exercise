7d484284231f5dc5434c4c5a4559632444188c60
Dheerajreddy9999 <56133508+Dheerajreddy9999@users.noreply.github.com>
2025-01-27 06:19:12 +0000

    initial commit

--- exercise/inventory/main.py
+++ exercise/inventory/main.py
@@ -1,6 +1,8 @@
 import openpyxl
 
-inv_file = openpyxl.load_workbook("inventory.xlsx")
-product_list = inv_file["sheet1"]
+inv_file = openpyxl.load_workbook("/workspaces/python-exercise/exercise/inventory/inventory.xlsx")
+product_list = inv_file["Sheet1"]
 
 products_per_supplier = {}
+
+for product_row in range(2, product_list.max_row):
