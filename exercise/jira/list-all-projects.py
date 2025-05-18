59691fa5ecbedb24d271152091ce2e800900e8e3
Dheerajreddy9999 <ksreddy.dheeraj@gmail.com>
2025-05-18 10:08:54 +0530

    updtae python exercise

--- exercise/jira/list-all-projects.py
+++ exercise/jira/list-all-projects.py
@@ -4,8 +4,7 @@ import json
 
 url = "https://ksreddydheeraj.atlassian.net/rest/api/3/project"
 
-API_TOKEN = "ATATT3xFfGF0tERAou7-WFYbEZcf-RNprcOyRnKUqADnsQBZPKXg2w2zsUIa73eiZGQVRIL8EDOh_6M1k3RZP0CjMpduGoQl4WLNglc7IKyQTj7un7JM7vvyYw_iEfyf1G5seWdM6bZN-0PbVC6CTBJOT4MYvQp0N5fK14Q2zsF2o3HXbGwgiNU=89E28883"
-
+API_TOKEN = ""
 auth = HTTPBasicAuth("ksreddy.dheeraj@gmail.com",API_TOKEN)
 
 headers = {
